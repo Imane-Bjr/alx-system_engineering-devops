d744de9db623367c05186f1928332a9c3f56306b
Imane-Bjr <boujirimane1@gmail.com>
2024-08-13 16:50:37 -0700

    task 1. Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,20 @@
 """
 number of subscribers 'How Many Subs?'
 """
-import requests as req
-headers = {"User-Agent": "User_Agent_/3.0"}
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Method Subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = req.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    return 0
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-agent'}  # Custom User-Agent to avoid Too Many Requests error
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
